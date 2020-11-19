4346d6d29d259a1cab38cc5791df6475c2b153da
alemagio <alessandro.magionami@gmail.com>
2020-11-19 17:21:56 +0100

    docs(examples): fixed examples index.html path

--- examples/basic/server.js
+++ examples/basic/server.js
@@ -10,7 +10,7 @@ app.register(socketio)
 app.get('/', async (req, reply) => {
   app.io.on('connect', () => console.info('Socket connected!'))
 
-  const data = await readFile(join(__dirname, 'index.html'))
+  const data = await readFile(join(__dirname, '..', 'index.html'))
   reply.header('content-type', 'text/html; charset=utf-8')
   reply.send(data)
 })
