a7692d2cd0043d26736b35de5d3a40a26117bacd
alemagio <alessandro.magionami@gmail.com>
2020-11-19 17:46:29 +0100

    test: added test for custom options scenario

--- test/index.test.js
+++ test/index.test.js
@@ -11,3 +11,30 @@ test('should register the correct decorator', async t => {
 
   t.deepEqual(app.io, require('socket.io')(app.server))
 })
+
+test('should register the correct decorator passing options', async t => {
+  t.plan(1)
+
+  const app = require('fastify')()
+
+  app.register(require('..'), {
+    path: '/test',
+    serveClient: false,
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+  })
+
+  await app.ready()
+
+  t.deepEqual(
+    app.io,
+    require('socket.io')(app.server, {
+      path: '/test',
+      serveClient: false,
+      pingInterval: 10000,
+      pingTimeout: 5000,
+      cookie: false
+    })
+  )
+})
