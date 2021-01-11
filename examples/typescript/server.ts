e68876a0156a5ce7d2e9a14426277b62865074f0
J. G. Sebring <johan.sebring@gmail.com>
2021-01-11 02:30:14 +0100

    fix: move io event handler to app.ready

--- examples/typescript/server.ts
+++ examples/typescript/server.ts
@@ -8,11 +8,15 @@ const app = fastify({ logger: true })
 app.register(socketioServer)
 
 app.get('/', async (req, reply) => {
-  app.io.on('connect', () => console.info('Socket connected!'))
-
   const data = await readFile(join(__dirname, '..', 'index.html'))
   reply.header('content-type', 'text/html; charset=utf-8')
   reply.send(data)
+})
+
+app.ready(err => {
+  if (err) throw err
+
+  app.io.on('connect', (socket) => console.info('Socket connected!', socket.id))
 })
 
 app.listen(3000)
