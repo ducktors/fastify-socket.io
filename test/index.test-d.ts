137a9556d186153492bd4d94ebdc9bd0e6f0ce7a
Kameyama Takumi <kamektx1@gmail.com>
2021-08-27 16:55:22 +0900

    Fix index.test-d.ts

--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -1,26 +1,30 @@
 import fastify from 'fastify'
 import socketioServer from '..'
 import { expectType, expectAssignable } from 'tsd'
-import { ServerOptions } from 'socket.io'
+import { Server, ServerOptions } from 'socket.io'
 
-try {
-  const app = fastify()
+const test = async () => {
+  try {
+    const app = fastify()
 
-  await app.ready()
+    await app.ready()
 
-  await app.register(socketioServer)
+    await app.register(socketioServer)
 
-  app.io.emit('test')
+    app.io.emit('test')
 
-  expectType<SocketIO.Server>(app.io)
+    expectType<Server>(app.io)
 
-  expectAssignable<Partial<ServerOptions>>({
-    path: '/test',
-    serveClient: false,
-    pingInterval: 10000,
-    pingTimeout: 5000,
-    cookie: false
-  })
-} catch (err) {
-  console.error(err)
+    expectAssignable<Partial<ServerOptions>>({
+      path: '/test',
+      serveClient: false,
+      pingInterval: 10000,
+      pingTimeout: 5000,
+      cookie: false
+    })
+  } catch (err) {
+    console.error(err)
+  }
 }
+
+test();
