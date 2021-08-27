93876bef3d0610c423314e83183424eded2e94d9
Kameyama Takumi <kamektx1@gmail.com>
2021-08-27 17:06:13 +0900

    Unwrap index.test-d.ts

--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -3,28 +3,24 @@ import socketioServer from '..'
 import { expectType, expectAssignable } from 'tsd'
 import { Server, ServerOptions } from 'socket.io'
 
-const test = async (): Promise<void> => {
-  try {
-    const app = fastify()
+try {
+  const app = fastify()
 
-    await app.ready()
+  await app.ready()
 
-    await app.register(socketioServer)
+  await app.register(socketioServer)
 
-    app.io.emit('test')
+  app.io.emit('test')
 
-    expectType<Server>(app.io)
+  expectType<Server>(app.io)
 
-    expectAssignable<Partial<ServerOptions>>({
-      path: '/test',
-      serveClient: false,
-      pingInterval: 10000,
-      pingTimeout: 5000,
-      cookie: false
-    })
-  } catch (err) {
-    console.error(err)
-  }
+  expectAssignable<Partial<ServerOptions>>({
+    path: '/test',
+    serveClient: false,
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+  })
+} catch (err) {
+  console.error(err)
 }
-
-await test()
