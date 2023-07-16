9ace17fba775fd442e3b3b7290924826ab8285d3
Muhammad Ramzan <ramzanm461@gmail.com>
2023-07-16 14:27:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 'use strict'
 
-const fp = require('fastify-plugin')
+import fp from "fastify-plugin";
+import { Server as SocketIOServer } from "socket.io";
 
-module.exports = fp(async function (fastify, opts) {
-  fastify.decorate('io', require('socket.io')(fastify.server, opts))
-  fastify.addHook('onClose', (fastify, done) => {
-    fastify.io.close()
-    done()
-  })
-}, { fastify: '>=3.24.x', name: 'fastify-socket.io' })
+module.exports = fp(
+    async function (fastify, opts) {
+        fastify.decorate("io", new SocketIOServer(fastify.server, opts));
+        fastify.addHook("onClose", async (_instance, done) => {
+            fastify.io.close();
+            done();
+        });
+    },
+      {
+        fastify: ">=4.x",
+        name: "fastify-socket.io",
+    }
+);
