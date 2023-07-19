d9471aeaae25f1b8cc3fb9610bf1ba2b4761dc83
Muhammad Ramzan <ramzanm461@gmail.com>
2023-07-19 13:50:33 +0000

    Fixed Lint Issues

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict'
 
-import fp from "fastify-plugin";
-import { Server as SocketIOServer } from "socket.io";
+import fp from 'fastify-plugin'
+import { Server as SocketIOServer } from 'socket.io'
 
 module.exports = fp(
-    async function (fastify, opts) {
-        fastify.decorate("io", new SocketIOServer(fastify.server, opts));
-        fastify.addHook("onClose", async (_instance, done) => {
-            fastify.io.close();
-            done();
-        });
-    },
-      {
-        fastify: ">=4.x",
-        name: "fastify-socket.io",
-    }
-);
+  async function (fastify, opts) {
+    fastify.decorate('io', new SocketIOServer(fastify.server, opts))
+    fastify.addHook('onClose', async (_instance, done) => {
+      fastify.io.close()
+      done()
+    })
+  },
+  {
+    fastify: '>=4.x',
+    name: 'fastify-socket.io'
+  }
+)
