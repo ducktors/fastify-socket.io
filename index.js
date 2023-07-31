0790776b0a7882fe5e9b15ef970f2cd4f5968fdc
Jorge Olaf Erlandsen <olaftriskel@gmail.com>
2023-07-31 14:32:28 -0400

    Update to fastify v4

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const fp = require('fastify-plugin')
+const socketIo = require('socket.io');
+
 
 module.exports = fp(async function (fastify, opts) {
-  fastify.decorate('io', require('socket.io')(fastify.server, opts))
+  fastify.decorate('io', new socketIo.SocketIOServer(fastify.server, opts))
   fastify.addHook('onClose', (fastify, done) => {
     fastify.io.close()
     done()
   })
-}, { fastify: '>=3.24.x', name: 'fastify-socket.io' })
+}, { fastify: '>=4.x', name: 'fastify-socket.io' })
