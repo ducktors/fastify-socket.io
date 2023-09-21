b3f4e8aa7d0af6818fe7c4d97bf2537ca79ca20a
Alessandro Magionami <alessandro.magionami@gmail.com>
2023-09-21 16:32:49 +0200

    chore: remove examples dir from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    outputFile: './junit.xml',
+    outputFile: "./junit.xml",
     coverage: {
       all: true,
-      reportsDirectory: 'coverage',
-      reporter: ['json', 'text-summary'],
-      provider: 'istanbul',
-      include: ['**/*.{ts,tsx}'],
-      exclude: ['template/**/*.{ts,tsx}'],
+      reportsDirectory: "coverage",
+      reporter: ["json", "text-summary"],
+      provider: "istanbul",
+      include: ["**/*.{ts,tsx}"],
+      exclude: ["template/**/*.{ts,tsx}", "examples/**/*.{ts,tsx}"],
     },
   },
-})
+});
